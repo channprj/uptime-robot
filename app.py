1a63ff0ec55f0e32c13b0dc8a0f0c2c71d07395f
CHANN <chann@chann.kr>
2016-08-04 17:23:38 +0900

    [UPDATE]: Add HTTP error exception and init some code.

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
 import sys
-from module import test
+from module import init 
 from flask import Flask
+from flask import render_template
+from flask import url_for
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return test.test_print()
+    return render_template('index.html')
+
+@app.route('/signup')
+def sign_up():
+    return 'WIP'
+
+@app.route('/signin')
+def sign_in():
+    return 'WIP'
+
+@app.route('/dashboard')
+def dashboard():
+    return 'WIP'
+
+@app.route('/test')
+def http_test():
+    return init.test_print()
+
+# error handler
+@app.errorhandler(404)
+def page_not_found(e):
+        return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
