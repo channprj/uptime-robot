8ec61b7f2e70890f302cc40e2757c5b2ffa2c633
CHANN <chann@chann.kr>
2016-08-05 17:13:54 +0900

    [UPDATE]: Init models.py file and clean up init code.

--- module/init.py
+++ module/init.py
@@ -5,42 +5,6 @@ import httplib
 import urllib
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://test.db'
-db = SQLAlchemy(app)
-
-### SQLAlchemy Data Type
-### Integer: an integer
-### String (size): a string with a maximum length
-### Text: some longer unicode text
-### DateTime: date and time expressed as Python datetime object.mroFloatstores floating point values
-### Boolean: stores a boolean value
-### PickleType: stores a pickled Python object
-### LargeBinary: stores large arbitrary binary data
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email_id = db.Column(db.String(120), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(20))
-    phone = db.Column(db.String(30))
-    is_admin = db.Column(db.Integer)
-    monitor = db.relationship('Monitor', backref='monitor', lazy='dynamic')
-
-class Monitor(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    event = db.Column(db.Boolean)
-    label = db.Column(db.String(20))
-    status = db.Column(db.Integer)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    monitor_log = db.relationship('MonitorLog', backref='monitor_log', lazy='dynamic')
-
-class MonitorLog(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    status = db.Column(db.Integer)
-    monitor_id = db.Column(db.Integer, db.ForeignKey('monitor.id'))
 
 class HTTPChecker(object):
     """docstring for HTTPChecker"""
