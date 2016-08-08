da863aeb0a7e1e8a4b509974ce74e416c1126bb7
CHANN <chann@chann.kr>
2016-08-08 18:30:40 +0900

    [ADD]: Init forms, views files and some template files.

--- module/views.py
+++ module/views.py
@@ -1,1 +1,16 @@
 # views.py
+from flask import render_template
+from flask import flash
+from flask import redirect
+import app
+from .forms import SignUpForm
+
+# init
+@app.route('/signup', methods=['GET', 'POST])
+def sign_in():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        flash('Sign In requested... ')
+        return redirect('/')
+
+    return render_template('sign_up.html', title='Sign In', form=form)
