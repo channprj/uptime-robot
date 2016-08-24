c16a0aef327562ac6ac15912d47fb4e9ccc17d90
CHANN <chann@chann.kr>
2016-08-24 01:54:49 +0000

    Fix: Add RadioField in forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import Form
 from wtforms import StringField
 from wtforms import PasswordField
+from wtforms import RadioField
 from wtforms.validators import DataRequired
 
 class SignUpForm(Form):
